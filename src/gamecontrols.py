e400280bef9663107ac910dd98c238c3779883c2
vh94 <valentinhaberl94@gmail.com>
2024-02-08 23:31:32 +0000

    added exceptions

--- src/gamecontrols.py
+++ src/gamecontrols.py
@@ -1,5 +1,14 @@
 # This file contains game control fucntions:
 
+import pytesseract
+from PIL import Image, ImageOps
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+# Setup tesseract OCR engine
+pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+tess_conf = '--psm 7 -c page_separator=''' 
+
 
 
 def restart_game(driver):
@@ -13,4 +22,34 @@ def restart_game(driver):
         .perform()
     
     pass
+
+def read_score(Individual_ID,trial,game_canvas):
+    ## OCR of score ##
+    # capture image:
+    game_canvas.screenshot(f'./data/img/{Individual_ID}_{trial}.png') # write to data/img
+
+    # IMPROVE: not read and write image but access from ram, ie:
+    # snap = game_canvas.screenshot_as_png 
+
+    # clean the image 
+    img = Image.open(f'./data/img/{Individual_ID}_{trial}.png')
+    roi = (240, 20, 295, 55) # (l,t,r,b) COULD BUG if double digit, cant test because qwop too hard ...
+    img_cropped = img.crop(roi)
+    img_bw = ImageOps.grayscale(img_cropped)
+    img_in = ImageOps.invert(img_bw)
+
+    img_in.save(f'./data/img/{Individual_ID}_{trial}_cropped.png')
+    # extract & store score as float
+    try:
+        score = pytesseract.image_to_string(img_in,config=tess_conf)
+        score = float(score.replace('\n',''))
+    except:
+        score = 0
+        print("score couldn't be read, setting 0")
+    # print("score is:",score)
+    # save cropped image for qc
+
+    return score
+
+
 
